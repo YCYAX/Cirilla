d2ecd884bb3f977d4c6cc58ac03dc57b3a7552cf
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-10 17:24:09 +0800

    添加注释

--- Cirilla/no_load/Log/__init__.py
+++ Cirilla/no_load/Log/__init__.py
@@ -2,6 +2,13 @@ import time
 
 
 def control(info_type: str, plugin_name: str, info: str):
+    """
+    打印日志到控制台
+
+    :param info_type: 信息提示类型
+    :param plugin_name: 输出的插件名称
+    :param info: 输出的提示信息
+    """
     time_info = time.strftime("%m-%d %H:%M:%S")
     print(f"\033[0;32;8m{time_info}\033[0m "
           f"[\033[1;37;8m{info_type}\033[0m] "
