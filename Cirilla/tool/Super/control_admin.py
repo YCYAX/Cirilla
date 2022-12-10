f54fd791985517170f615a6a5f9048daf8d90683
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-10 21:33:13 +0800

    add_super添加被操作人

--- Cirilla/tool/Super/control_admin.py
+++ Cirilla/tool/Super/control_admin.py
@@ -1,16 +1,16 @@
-import json
 from Cirilla.tool.Super import SUPER_ADMIN, save_file
 
 path = "src/Super/admin.json"
 
 
-def add_super(user_id: int):
+def add_super(user_id: int, control: str = "super"):
     """
     添加bot超管用户
 
     :param user_id: 玩家qq
+    :param control: 操作玩家
     """
     SUPER_ADMIN.update({
-        user_id
+        control: user_id
     })
     save_file()
