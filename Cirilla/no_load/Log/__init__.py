a7b326437e8915d507b0b9c45b31defdcb6686a7
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-10 17:11:37 +0800

    添加control用于打印日志

--- Cirilla/no_load/Log/__init__.py
+++ Cirilla/no_load/Log/__init__.py
@@ -3,4 +3,7 @@ import time
 
 def control(info_type: str, plugin_name: str, info: str):
     time_info = time.strftime("%m-%d %H:%M:%S")
-    print(f"{time_info} [{info_type} {plugin_name} | {info}")
+    print(f"\033[0;32;8m{time_info}\033[0m "
+          f"[\033[1;37;8m{info_type}\033[0m] "
+          f"\033[4;36;8m{plugin_name}\033[0m "
+          f"| {info}")
