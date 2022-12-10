ebb13db968b6a1b802b4ee70892a5b28ecb96ed3
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-10 20:50:07 +0800

    添加注释

--- Cirilla/tool/Money/control_money.py
+++ Cirilla/tool/Money/control_money.py
@@ -5,6 +5,12 @@ path = "tool/Money/money.json"
 
 
 def add_money(user_id: int, money_mount: int):
+    """
+    给玩家添加金钱
+
+    :param user_id: 玩家qq
+    :param money_mount: 金钱数量
+    """
     old_mount = PLAYER_MONEY[user_id]
     new_mount = old_mount + money_mount
     PLAYER_MONEY[user_id] = new_mount
