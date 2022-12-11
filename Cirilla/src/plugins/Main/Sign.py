593b476304d4f0a2d09a42221cb24107c9627df4
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-11 12:35:08 +0800

    增加时间判断

--- Cirilla/src/plugins/Main/Sign.py
+++ Cirilla/src/plugins/Main/Sign.py
@@ -2,7 +2,7 @@ from nonebot import on_command
 from Cirilla.tool.Money import PLAYER_MONEY
 from Cirilla.tool.Money.control_money import *
 import nonebot.adapters.onebot.v11 as v11
-import random
+import random,time
 
 sign = on_command("签到", priority=5, block=True)
 
@@ -17,14 +17,26 @@ async def player_sign(group_event: v11.GroupMessageEvent):
     # 获取玩家名字和id
     player_id = group_event.user_id
     player_name = group_event.sender.nickname
-    # 生成金币数量
+    # 生成金币数量和签到时间
     money_mount = random.randint(0, 100)
-    # 添加金币
-    if PLAYER_MONEY.get(player_id):
-        add_money(player_id, money_mount)
+    time_info = time.strftime("%y-%m-%d")
+    # 判断是否首次签到
+    try:
+        player_last_sign_time = PLAYER_MONEY.get(player_id)["签到时间"]
+    except TypeError:
+        player_last_sign_time = time_info + "1"
+    # 判断签到时间
+    if player_last_sign_time < time_info:
+        add_money(player_id, money_mount, time_info)
+        await sign.send(f"{player_name} 签到成功\n给你{money_mount}枚金币")
+    elif player_last_sign_time == time_info:
+        await sign.send(f"{player_name} 你今天签到过了")
     else:
         PLAYER_MONEY.update({
-            player_id: money_mount
+            player_id: {
+                "签到时间": time_info,
+                "金币数量": money_mount
+            }
         })
-    await sign.send(f"{player_name} 签到成功\n给你{money_mount}枚金币")
+        await sign.send(f"{player_name} 签到成功\n给你{money_mount}枚金币")
 
--- Cirilla/tool/Money/control_money.py
+++ Cirilla/tool/Money/control_money.py
@@ -4,13 +4,15 @@ from Cirilla.tool.Money import PLAYER_MONEY
 path = "tool/Money/money.json"
 
 
-def add_money(user_id: int, money_mount: int):
+def add_money(user_id: int, money_mount: int, time_info:str):
     """
     给玩家添加金钱
 
     :param user_id: 玩家qq
     :param money_mount: 金钱数量
+    :param time_info: 签到时间
     """
-    old_mount = PLAYER_MONEY[user_id]
+    old_mount = PLAYER_MONEY[user_id]["金币数量"]
     new_mount = old_mount + money_mount
-    PLAYER_MONEY[user_id] = new_mount
+    PLAYER_MONEY[user_id]["金币数量"] = new_mount
+    PLAYER_MONEY[user_id]["签到时间"] = time_info
