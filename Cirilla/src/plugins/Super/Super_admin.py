222aa865b6ed46b33b54756b97057046d26cdd34
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-11 12:36:25 +0800

    修复TypeError

--- Cirilla/src/plugins/Super/Super_admin.py
+++ Cirilla/src/plugins/Super/Super_admin.py
@@ -1,6 +1,6 @@
 from nonebot import on_command
 from nonebot.rule import to_me
-from nonebot.params import ArgPlainText
+from nonebot.params import ArgStr
 from Cirilla.tool.Super import SUPER_ADMIN
 from Cirilla.tool.Super.control_admin import add_super
 import nonebot.adapters.onebot.v11 as v11
@@ -18,7 +18,7 @@ async def check_is_first_add():
 
 
 @add.got("add", prompt="输入一个想要添加的超管qq号")
-async def add_super_admin(private_event: v11.PrivateMessageEvent, add_id: int = ArgPlainText("add")):
+async def add_super_admin(private_event: v11.PrivateMessageEvent, add_id: int = ArgStr("add")):
     """
     添加超管用户
 
@@ -30,9 +30,9 @@ async def add_super_admin(private_event: v11.PrivateMessageEvent, add_id: int = 
     # 判断是否为超管
     if player_id in SUPER_ADMIN:
         # 添加
-        add_super(add_id, str(player_id))
+        add_super(int(add_id), str(player_id))
         await add.finish(f"添加用户{add_id}为超管")
     else:
         # 添加
-        add_super(add_id)
+        add_super(int(add_id))
         await add.finish(f"添加用户{add_id}为超管")
