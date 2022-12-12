0cb75d2e3eec9df9b70e7ba321a47b3055badde0
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-12 17:59:39 +0800

    add 查询玩家金钱数量 函数

--- Cirilla/tool/Money/control_money.py
+++ Cirilla/tool/Money/control_money.py
@@ -3,7 +3,7 @@ from Cirilla.tool.Money import PLAYER_MONEY
 path = "tool/Money/money.json"
 
 
-def add_money(user_id: str, money_mount: int, time_info:str):
+def add_money(user_id: str, money_mount: int, time_info: str):
     """
     给玩家添加金钱
 
@@ -15,3 +15,14 @@ def add_money(user_id: str, money_mount: int, time_info:str):
     new_mount = old_mount + money_mount
     PLAYER_MONEY[user_id]["金币数量"] = new_mount
     PLAYER_MONEY[user_id]["签到时间"] = time_info
+
+
+def query_money(user_id: str) -> int:
+    """
+    查询玩家金钱
+
+    :param user_id: 玩家qq
+    :return: 金币数量
+    """
+    mount = PLAYER_MONEY[user_id]["金币数量"]
+    return mount
