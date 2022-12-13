82a0570ba6264239ceb61b45e6a0b00df77e9f4f
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-13 15:33:17 +0800

    更新注释

--- Cirilla/tool/Menu/control_menu.py
+++ Cirilla/tool/Menu/control_menu.py
@@ -5,7 +5,12 @@ show_menu_to_player() -> 给玩家展示菜单
 from Cirilla.tool.Conversion import image_base64
 
 
-def show_menu_to_player(menu_type: str):
+def show_menu_to_player(menu_type: str) -> str:
+    """
+    给玩家展示菜单
+    :param menu_type: 菜单类型
+    :return: cq码
+    """
     path = "photo/output/" + menu_type + ".jpg"
     image = image_base64(path)
     cq_str = "[CQ:image,file=base64://" + image + "]"
