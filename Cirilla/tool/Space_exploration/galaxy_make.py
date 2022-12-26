55e0be28b2bc9c26815bbe417b42a24d848fed86
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-26 16:37:58 +0800

    修复bug

--- Cirilla/tool/Space_exploration/galaxy_make.py
+++ Cirilla/tool/Space_exploration/galaxy_make.py
@@ -10,9 +10,9 @@ class Galaxy:
     def __init__(self, first_name):
         # 随机后名字
         range_number = random.randint(2, 8)
-        last_name = ""
         yes_or_no = True
         while yes_or_no:
+            last_name = ""
             for i in range(range_number):
                 index = random.randint(0, 35)
                 last_name += "QWERTYUIOPASDFGHJKLZXCVBNM0147852369"[index]
