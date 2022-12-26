b9a49c9bbe83f6a65bd7605cea80f37ad7ac7507
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-26 13:52:30 +0800

    增加星系生成

--- Cirilla/tool/Space_exploration/__init__.py
+++ Cirilla/tool/Space_exploration/__init__.py
@@ -1,26 +1,41 @@
 """
 本模块负责处理玩家宇宙探索相关业务
 读取所有玩家注册信息: SPACE_SIGN
+读取所有星球信息: SPACE_PLANET
 SPACE_SIGN结构
 {
-    “id”："名称"
+    "id": {
+        "name": "玩家昵称",
+        "pos": "星系名字",
+        "state": "玩家状态"
+    }
 }
-
+SPACE_GALAXY结构
+{
+    "星系名字": {
+        "galaxy_pos": [宇宙坐标],
+        "galaxy_size": 星系内星球数量
+    }
+}
 函数：
-
-
+load_file -> 加载json到内存
+save_file -> 保存内容至json
 模块：
-
+galaxy_make.py -> 生成星系
 """
 import json, os
 from Cirilla.tool.Log import control
 
-path = "tool/Space_exploration/player_sign.json"
+player_sign_path = "tool/Space_exploration/player_sign.json"
+planet_path = "tool/Space_exploration/planet.json"
+galaxy_path = "tool/Space_exploration/galaxy.json"
+
+path_list = [planet_path, galaxy_path, player_sign_path]
 
 
-def load_file() -> dict:
+def load_file(path) -> dict:
     """
-    加载money.json内容到内存
+    加载json到内存
     """
     with open(path, "r", encoding="utf8") as file:
         info = file.read()
@@ -28,22 +43,33 @@ def load_file() -> dict:
         return player_json
 
 
-def save_file():
+def save_file(path,info):
     """
-    保存内容至money.json
+    保存内容至json
     """
     with open(path, "w", encoding="utf8") as file:
-        info_json = json.dumps(SPACE_SIGN, ensure_ascii=False)
+        info_json = json.dumps(info, ensure_ascii=False)
         file.write(info_json)
 
 
-# 判断路径
-if os.path.exists(path):
-    # 读取
-    SPACE_SIGN = load_file()
-else:
-    control("SUCCESS", "Space", "Space包检测到缺少player_sign.json,已创建")
-    with open(path, "w+", encoding="utf8") as f:
-        f.write(json.dumps({}))
-    # 读取
-    SPACE_SIGN = load_file()
+for path in path_list:
+    # 判断路径
+    if os.path.exists(path):
+        # 读取
+        if path == player_sign_path:
+            SPACE_SIGN = load_file(path)
+        elif path == planet_path:
+            SPACE_PLANET = load_file(path)
+        elif path == galaxy_path:
+            SPACE_GALAXY = load_file(path)
+    else:
+        control("SUCCESS", "Space", "Space包检测到缺少" + path + "已创建")
+        with open(path, "w+", encoding="utf8") as f:
+            f.write(json.dumps({}))
+        # 读取
+        if path == player_sign_path:
+            SPACE_SIGN = load_file(path)
+        elif path == planet_path:
+            SPACE_PLANET = load_file(path)
+        elif path == galaxy_path:
+            SPACE_GALAXY = load_file(path)
