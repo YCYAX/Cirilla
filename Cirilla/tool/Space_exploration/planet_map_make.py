222e89a9299eb9cd279512496a4684f9c101011b
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-26 21:25:50 +0800

    生成地图工具

---
+++ Cirilla/tool/Space_exploration/planet_map_make.py
@@ -0,0 +1,38 @@
+import numpy, random
+import array
+
+
+def make_map(size: int) -> array.array:
+    """
+    生成地图
+
+    :param size: 地图尺寸大小
+
+    :return: 地图数值
+    """
+    # 生成空数组
+    map = numpy.array([])
+
+    def make_map_row(size: int) -> list:
+        """
+        生成地图的行
+
+        :param size: 地图尺寸大小
+
+        :return: 地图行数值
+        """
+        # 生成文字
+        txt = "金木水火土"
+        # 空行列表
+        map = []
+        # 生成一行
+        for i in range(size):
+            index = random.randint(0, 4)
+            map.append(txt[index])
+        return map
+    # 生成所有地图-列
+    for i in range(size):
+        map = numpy.append(map, [make_map_row(size)])
+    # 确定形状
+    map = numpy.reshape(map, (size, size))
+    return map
