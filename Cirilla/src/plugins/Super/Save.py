8d412fe3c66774a1d9b0accc79267847a04e5df0
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-26 13:41:45 +0800

    增加 玩家注册，星系，星球保存数据

--- Cirilla/src/plugins/Super/Save.py
+++ Cirilla/src/plugins/Super/Save.py
@@ -1,6 +1,7 @@
 from nonebot import on_command
 from nonebot.rule import to_me
-from Cirilla.tool.Money import save_file
+from Cirilla.tool.Money import save_file as money_save
+from Cirilla.tool.Space_exploration import *
 from Cirilla.tool.Super import SUPER_ADMIN
 import nonebot.adapters.onebot.v11 as v11
 
@@ -16,7 +17,10 @@ async def save_data(private_event: v11.PrivateMessageEvent):
     """
     # 判断是否为超管
     if int(private_event.user_id) in SUPER_ADMIN.values():
-        save_file()
-        await save.finish("所有玩家金币保存成功")
+        money_save()
+        save_file(planet_path, SPACE_PLANET)
+        save_file(galaxy_path, SPACE_GALAXY)
+        save_file(player_sign_path, SPACE_SIGN)
+        await save.finish("所有数据保存成功")
     else:
         await save.finish("你权限不足")
