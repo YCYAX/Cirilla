0f562ceb378b0eb8a0298a5311845682fecb1fe9
YCYAX <87980311+YCYAX@users.noreply.github.com>
2022-12-26 13:41:19 +0800

    调整逻辑

--- Cirilla/src/plugins/Space_exploration/Space_sign.py
+++ Cirilla/src/plugins/Space_exploration/Space_sign.py
@@ -4,10 +4,11 @@ import nonebot.adapters.onebot.v11 as v11
 
 sign = on_command("登录宇宙", priority=1, block=True)
 
+
 @sign.handle()
 async def player_sign(group_event: v11.GroupMessageEvent):
     """
-    玩家签到
+    玩家注册
 
     :param group_event: 群聊事件
     """
@@ -16,17 +17,23 @@ async def player_sign(group_event: v11.GroupMessageEvent):
     player_name = group_event.sender.nickname
     # 判断是否首次登录
     if player_id in SPACE_SIGN:
-        await sign.finish(f"欢迎回来！ {SPACE_SIGN[player_id]} 冒险者")
+        await sign.send(f"欢迎回来！ {SPACE_SIGN[player_id]['name']} 冒险者")
     else:
+        # 判断用户名是否重复
         if player_name in SPACE_SIGN.values():
             SPACE_SIGN.update({
-                player_id: player_name + "#" + player_id[-4:]
+                player_id: {
+                    "name": player_name + "#" + player_id[-4:],
+                    "pos": None,
+                    "state": "sign"
+                }
             })
-            save_file()
         else:
             SPACE_SIGN.update({
-                player_id: player_name
+                player_id: {
+                    "name": player_name,
+                    "pos": None,
+                    "state": "sign"
+                }
             })
-            save_file()
-        await sign.send(f"欢迎加入宇宙！ {SPACE_SIGN[player_id]} 冒险者，回复 宇宙帮助 来开始探索吧")
-
+        await sign.send(f"欢迎加入宇宙！ {SPACE_SIGN[player_id]['name']} 冒险者，回复 宇宙帮助 来开始探索吧")
