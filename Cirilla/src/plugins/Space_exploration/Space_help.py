79ed8c2cf1f5ce2cf8901acba94e38303d8ccf23
YCYAX <87980311+YCYAX@users.noreply.github.com>
2023-01-01 20:12:36 +0800

    增加 宇宙帮助 内容

--- Cirilla/src/plugins/Space_exploration/Space_help.py
+++ Cirilla/src/plugins/Space_exploration/Space_help.py
@@ -1,19 +1,19 @@
 from nonebot import on_command
-import nonebot.adapters.onebot.v11 as v11
 
 
 help = on_command("宇宙帮助", priority=1, block=True)
 
 
 @help.handle()
-async def help_show(group_event: v11.GroupMessageEvent):
+async def help_show():
     """
     帮助菜单
-
-    :param group_event: 群聊事件
     """
     txt = "|登录宇宙|加入宇宙探索游戏|\n" \
           "|进入星系|进入一个星系|\n" \
           "|进入星球|进入一个星球|\n" \
-          "|星系信息|查看当前星系信息|\n"
+          "|星系信息|查看当前星系信息|\n" \
+          "|移动|在星球上移动位置|\n" \
+          "|离开星球|离开星球回到星系|"
+
     await help.finish(f"{txt}")
